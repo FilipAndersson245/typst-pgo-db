66efb88dec728034007af10ea25eb210f30e1494
FilipAndersson245 <17986183+FilipAndersson245@users.noreply.github.com>
2023-07-22 00:15:56 +0200

    ensure profile is keep between bin runs, also run tests first for better coverage.

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 echo "Sampling started"
+
+cargo clean
+cargo pgo instrument test -- --workspace --test tests
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-find $SCRIPT_DIR -type f -regex '.*/\(\(main\|example\|demo\).typ\|gallery/.*.typ\|examples/.*.typ\|example/.*.typ\)' | while read line ; do echo === $line === && $1 c $line /tmp/foo.pdf; done
+find $SCRIPT_DIR -type f -regex '.*/\(\(main\|example\|demo\).typ\|gallery/.*.typ\|examples/.*.typ\|example/.*.typ\)' | \
+while read line ; do echo === $line === && cargo pgo instrument --keep-profiles run -- c $line /tmp/foo.pdf; done
 
 echo "Sampling done now running pgo optimize"
 cargo pgo optimize
