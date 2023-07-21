2dc67411f2d247d34e6a1df828bfb08961adb397
FilipAndersson245 <17986183+FilipAndersson245@users.noreply.github.com>
2023-07-21 23:10:50 +0200

    fixes some regex misses.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Sampling started"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-find $SCRIPT_DIR -type f -regex '.*/\(\(main\|example\|demo\).typ\|gallery/*.typ\|examples/*.typ\)' | while read line ; do echo === $line === && $1 c $line /tmp/foo.pdf; done
+find $SCRIPT_DIR -type f -regex '.*/\(\(main\|example\|demo\).typ\|gallery/.*.typ\|examples/.*.typ\|example/.*.typ\)' | while read line ; do echo === $line === && $1 c $line /tmp/foo.pdf; done
 
 echo "Sampling done now running pgo optimize"
 cargo pgo optimize
